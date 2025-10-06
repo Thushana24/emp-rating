4714a354ea6c8f76f25559c440ea0309b1d5abbc
Thushana LS <dhushanals@example.com>
2025-10-06 08:20:49 +0530

    feat(auth): add accept-invite endpoint and related types
    - Implemented `POST /api/auth/accept-invite` to handle invitation acceptance

---
+++ src/app/api/auth/types.ts
@@ -0,0 +1,24 @@
+import { LoginUserSchema, RegisterUserSchema } from "@/schemas/user.schema";
+import { Prisma, User } from "@prisma/client";
+import z from "zod";
+
+export type UserWithToken = User & { token: string };
+
+export type RegisterInput = z.infer<typeof RegisterUserSchema>;
+
+export type LoginInput = z.infer<typeof LoginUserSchema>;
+
+export type UserWithRelations = Prisma.UserGetPayload<{
+  select: {
+    id: true;
+    email: true;
+    firstName: true;
+    lastName: true;
+    createdAt: true;
+    updatedAt: true;
+    OrganizationMembers: true;
+    OwnedOrganizations: true;
+    RatingsAsEmployee: true;
+    RatingsAsSupervisor: true;
+  };
+}>;
